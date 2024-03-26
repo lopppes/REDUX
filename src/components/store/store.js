47151a9d00c04c31640991092f0092da89877e8b
lopppes <sl263920@gmail.com>
2024-03-26 15:02:35 -0300

    first commit

---
+++ src/components/store/store.js
@@ -0,0 +1,15 @@
+
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './reducers/cart'
+
+
+const store = configureStore(
+    {
+        reducer: {
+            cartProducts: cartReducer,
+        },
+    }
+)
+
+
+export default store
